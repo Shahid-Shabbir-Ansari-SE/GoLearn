e309b242f6196707096d2576fd066c96d499e6ec
Shahid-Shabbir-Ansari-SE <shahidshabbirse@gmail.com>
2024-03-09 15:54:17 +0500

    feat: Implement instructor creation endpoint and course addition functionality

---
+++ server/src/types/instructor.types.ts
@@ -0,0 +1,7 @@
+export interface instructorModel{
+    instructorImageURL: string,
+    instructorName: string,
+    instructorPosition: string,
+    instructorAbout: string,
+    courses?: Array<string>
+}
