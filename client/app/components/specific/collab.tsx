5afcaba23bb2175a164ee0cd83fe7752c7747775
Shahid-Shabbir-Ansari-SE <shahidshabbirse@gmail.com>
2024-03-13 12:24:50 +0500

    fix - removed client folder

--- client/app/components/specific/collab.tsx
+++
@@ -1,8 +0,0 @@
-export default function Collab() {
-  return (
-    <div className='font-interSemiBold mt-4 justify-center space-y-12 px-32 text-center text-2xl'>
-      <h1>We collaborate with 50+ leading institutions and companies</h1>
-      <img src='/images/homePage/collab.png' alt='' />
-    </div>
-  )
-}
