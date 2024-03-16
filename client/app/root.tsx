3b7a9212d99279865e023323e38e08626c50fcd0
Shahid-Shabbir-Ansari-SE <shahidshabbirse@gmail.com>
2024-03-16 10:12:54 -0700

    feat - Add user authentication

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -1,33 +1,36 @@
-import type { LinksFunction } from "@remix-run/node";
-import stylesheet from "./tailwind.css";
+import type { LinksFunction } from '@remix-run/node'
+import stylesheet from './tailwind.css'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
+import Navbar from '../app/components/layout/navBar'
 
 export const links: LinksFunction = () => [
-    { rel: "stylesheet", href: stylesheet },
-];
+  { rel: 'stylesheet', href: stylesheet }
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
+        <link rel='shortcut icon' href='/favicon.svg' type='image/x-icon' />
       </head>
       <body>
+        <Navbar />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
